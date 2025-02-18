68d2c4366c627772e9789dcfe94b7feb3c6b918d
jenarsaX <u21316662@utp.edu.pe>
2025-02-18 00:07:07 -0500

    DepartmentController

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,62 +4,80 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DepartmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $department = Department::all();
+
+        return response()->json([
+            'success' => true,
+            'data' => $department,
+        ],200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $department = Department::create([
+            'name' => $request->input('name'),
+            'tamaño' => $request->input('tamaño'),
+            'precio' => $request->input('precio'),
+            'estatus' => $request->input('estatus'),
+            'description' => $request->input('description'),
+            'fecha_agregado' => $request->input('fecha_agregado'),
+            'pisos' => $request->input('pisos'),
+            'id_administrador' => Auth::user()->id,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Creado correctamente',
+            'data' => $department
+        ],201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Department $department)
+    public function show(string $id)
     {
-        //
+        $department = Department::find($id);
+
+        return response()->json([
+            'success' => true,
+            'data' => $department
+        ],200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Department $department)
+    public function update(Request $request, string $id)
     {
-        //
+        $department = Department::find($id);
+        if(!$department){
+            return response()->json(['message' => 'No encontrado'], 404);
+        }
+
+        $department->name = $request->input('name');
+        $department->tamaño = $request->input('tamaño');
+        $department->precio = $request->input('precio');
+        $department->estatus = $request->input('estatus');
+        $department->description = $request->input('description');
+        $department->fecha_agregado = $request->input('fecha_agregado');
+        $department->pisos = $request->input('pisos');
+        $department->id_administrador = Auth::user()->id;
+        $department->save();
+
+        return response()->json([
+            'success' => true,
+            'data' => $department,
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Department $department)
+    public function destroy(string $id)
     {
-        //
-    }
+        Department::find($id)->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Department $department)
-    {
-        //
+        return response()->json([
+            'success' => true,
+            'message' => 'Eliminado correctamente',
+        ],200);
     }
 }
